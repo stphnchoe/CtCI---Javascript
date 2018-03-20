b722620b116926ff54831218dee0fa827aa57c0b
Stephen Choe <stphnchoe@gmail.com>
2018-03-20 02:01:33 -0700

    Attempt 1 IsUnique with data structure - SC

--- Chapter 1 Arrays and Strings/IsUnique.js
+++ Chapter 1 Arrays and Strings/IsUnique.js
@@ -1,9 +1,17 @@
 var uniqueChar = function(string) {
-    var obj = {};
-    for (var i = 0; i < string.length; i++) {
-        if (!obj[string[i]]) {
-            obj[string[i]] = true;
-        } else return false
+    var str = string.toLowerCase();
+    var duplicateCheck = {};
+    for (var i = 0; i < str.length; i++) {
+        if ( duplicateCheck[str[i]] ) {
+            return false;
+        }
+        duplicateCheck[str[i]] = string[i]
     }
     return true;
 }
+
+console.log(uniqueChar('Hello'));
+console.log(uniqueChar('Helloh'));
+console.log(uniqueChar('heloh'));
+console.log(uniqueChar('helo'));
+console.log(uniqueChar('Bb'));
