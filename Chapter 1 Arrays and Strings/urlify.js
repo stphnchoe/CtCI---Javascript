7cec8d983696c2736de0d3232da1066cec705b26
Stephen Choe <stphnchoe@gmail.com>
2018-03-21 16:03:43 -0700

    Completes URLify function

--- Chapter 1 Arrays and Strings/urlify.js
+++ Chapter 1 Arrays and Strings/urlify.js
@@ -1,5 +1,15 @@
 var URLify = function(string) {
-	return string.split(' ').join('%20');
+	var url = '';
+
+	for (var i = 0; i < string.length; i++) {
+		if ( string[i] === ' ' && string[i + 1] !== ' ' ) {
+			url += '%20';
+		} else if ( string[i] !== ' ' ) {
+			url += string[i];
+		}
+	}
+	return url;
 }
 
 console.log(URLify('Mr John Smith'));
+console.log(URLify('what about  more    huh'));
