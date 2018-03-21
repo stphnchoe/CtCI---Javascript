01707152b1470631fafd0fbd6b873c5f1b232ce1
Stephen Choe <stphnchoe@gmail.com>
2018-03-21 15:29:50 -0700

    Completes functions for solving base case of  whether a string is a permutation of another string

--- Chapter 1 Arrays and Strings/isPermutation.js
+++ Chapter 1 Arrays and Strings/isPermutation.js
@@ -1,12 +1,27 @@
 var isPermutation= function(first, second) {
-    if ( first.length !== second.length) {
+    var string1 = {};
+    var string2 = {};
+
+    //string.toLowerCase() each string
+
+    //whitespace omitt or count
+
+    //needs to have same length
+    if (first.length !== second.length) {
         return false;
     }
-    
-    sorted1 = first.split('').sort().join('');
-    sorted2 = second.split('').sort().join('');
+    //needs to have same characters
+    for (var i = 0; i < first.length; i++) {
+        string1[first[i]] = ++string1[first[i]] || 1;
+        string2[second[i]] = ++string2[second[i]] || 1;
+    }
 
-    return sorted1 === sorted2;
-
+    for (var key in string1) {
+        if (string1[key] !== string2[key]) {
+            return false;
+        }
+    }
+    return true;
 }
-console.log(isPermutation('dog', 'god'))
+console.log(isPermutation('dog', 'god'));
+console.log(isPermutation('', ''));
